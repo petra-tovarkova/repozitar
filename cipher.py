a10a30bcd818c3250b6c871b54d0c1706d73507b
petratovarkova <tovarkovapetra@centrum.cz>
2024-02-17 11:17:47 +0100

    Add cipher.py

---
+++ cipher.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+
+def duplicate(plaintext: str) -> str:
+    """
+    Duplicate chars in 'plaintext':
+    duplicate('hello') -> 'hheelloo'
+    """
+    # TODO
+    pass
+
+
+def intersection(a: str, b: str) -> str:
+    """
+    Return intersectioned string
+    intersection('zirafa', 'karafa') -> 'rafa'
+    """
+    # TODO
+    pass
+
+
+def caesar(plaintext: str, move: int) -> str:
+    """
+    Move 'plaintext' by 'move' characters. 'move' could be negative
+    caesar('hello', 1) -> 'ifmmp'
+    """
+    # TODO
+    pass
+
+
+def main() -> None:
+    # TODO
+    pass
+
+
+if __name__ == '__main__':
+    main()
